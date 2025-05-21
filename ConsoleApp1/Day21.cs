e418672f9efb73c938f013e10468a678be5d309d
Technohubjk <sosshivam65@gmail.com>
2025-05-21 10:55:58 +0530

    Add project files.

---
+++ ConsoleApp1/Day21.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Day21
+    {
+        public void IsPlaindrom()
+        {
+            string str = "NAMAN";
+            int i = 0;
+            int j = str.Length - 1;
+            bool IsPalindrom = true;
+            while (i < j)
+            {
+                if (str[i] != str[j])
+                {
+                    IsPalindrom = false;
+                    Console.WriteLine("This is not Palindrom string");
+                    break;
+                }
+                i++;
+                j--;
+               
+            }
+            if (IsPalindrom)
+            {
+                Console.WriteLine("This is Palindrim String " + str);
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day21 obj = new Day21();
+            obj.IsPlaindrom();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
